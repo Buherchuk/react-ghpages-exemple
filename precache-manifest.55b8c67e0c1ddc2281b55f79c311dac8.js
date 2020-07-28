4ec5e772b4df1dd37f4a23934df644637c0acbdd
Bohdan Buherchuk <bugerchuk@kpnu.km.ua>
2020-07-28 11:35:39 +0300

    Updates

---
+++ precache-manifest.55b8c67e0c1ddc2281b55f79c311dac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51e4d02025e55914bbe247b99b9f86d",
+    "url": "/react-ghpages-exemple/index.html"
+  },
+  {
+    "revision": "f9f7e8e79afba3c79210",
+    "url": "/react-ghpages-exemple/static/css/main.dc9d8e90.chunk.css"
+  },
+  {
+    "revision": "e2e90d55ba24a1fa1f42",
+    "url": "/react-ghpages-exemple/static/js/2.b685aa7f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-ghpages-exemple/static/js/2.b685aa7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f7e8e79afba3c79210",
+    "url": "/react-ghpages-exemple/static/js/main.4d79fe0c.chunk.js"
+  },
+  {
+    "revision": "aae87c213d1fcfe844a5",
+    "url": "/react-ghpages-exemple/static/js/runtime-main.a699b2ba.js"
+  }
+]);
